edf5665567cbecd550fa3d91c2beaee4b77b69b2
jvscJoao <joaov1500@gmail.com>
2024-12-18 15:12:27 -0300

    first commit

---
+++ src/main/java/local/project/bar/projetobar/ProjetobarApplication.java
@@ -0,0 +1,13 @@
+package local.project.bar.projetobar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetobarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetobarApplication.class, args);
+	}
+
+}
---
+++ src/test/java/local/project/bar/projetobar/ProjetobarApplicationTests.java
@@ -0,0 +1,13 @@
+package local.project.bar.projetobar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetobarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
